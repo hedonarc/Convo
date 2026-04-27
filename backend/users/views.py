423ac7f57df4114821e3fbb673c65799dfa17d3d
msulemanb <msulemanbutt39@gmail.com>
2026-04-28 01:48:17 +0500

    added the loginview and endpoint also used authenticate and refreshtoken

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class ProtectedView(APIView):
@@ -47,3 +49,30 @@ class RegisterView(APIView):
         return Response(
             data={"message": "You are registered"}, status=status.HTTP_201_CREATED
         )
+
+class LoginView(APIView):
+    def post(self, request):
+        username_email = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username_email or not password:
+            return Response(
+                data={"message": "Username/Email & Password required."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Email hai ya nai check kr rha hu
+        if "@" in username_email:
+            try:
+                username_email = User.objects.get(email=username_email).username
+
+            except User.DoesNotExist:
+                return Response({"message": "Invalid credentials"},
+                    status=status.HTTP_401_UNAUTHORIZED)
+
+        user = authenticate(username=username_email, password=password)
+        if not user:
+            return Response({"message": "Invalid Credentials"},status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({"access":str(refresh.access_token), "refresh":str(refresh)})
