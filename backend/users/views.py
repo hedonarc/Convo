4fe4078888c6c9cb2d10ed91945799e5b54ac68c
msulemanb <msulemanbutt39@gmail.com>
2026-04-27 17:56:57 +0500

    added jwt and some users

---
+++ backend/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "You are authenticated"})
