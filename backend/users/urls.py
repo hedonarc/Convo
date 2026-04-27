d54f4f69f56ac171e91b32864249b8fc7a6518eb
Abubakar Khawaja <abubakark1998@gmail.com>
2026-04-27 21:04:31 +0500

    feat: add register api (#3)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from users.views import ProtectedView
+from users.views import RegisterView
 
 urlpatterns = [
     path("protected/", ProtectedView.as_view()),
+    path("register/", RegisterView.as_view()),
 ]
