d3e793ffd2e95a81690dd21692b07d48a0b7cb0b
Abubakar Khawaja <abubakarkhawaja@hotmail.com>
2026-04-28 14:40:18 +0500

    Add Register User Api  (#7)
    * fix: add djangorestframework-simplejwt to project dependencies
    
    * feat: add management command for admin setup and gitignore cleanup
    
    * feat: add register api (#3)
    
    * chore: run ruff format
    
    * add user  and add doc string for setup_admin command
    
    * added loginview + login endpoint + return user, access, refresh & user in response (#9)
    
    * added the loginview and endpoint also used authenticate and refreshtoken
    
    * added user object in response of login api
    
    * synced the response with register api
    
    * applied format
    
    * use user serializer
    
    ---------
    
    Co-authored-by: Suleman <msulemanbutt39@gmail.com>

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from users.views import ProtectedView
+from users.views import ProtectedView, RegisterView, LoginView
 
 urlpatterns = [
     path("protected/", ProtectedView.as_view()),
+    path("register/", RegisterView.as_view()),
+    path("login/", LoginView.as_view()),
 ]
