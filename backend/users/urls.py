5b530dbb62f42759015f76a13792c4ca90d1a165
msulemanb <msulemanbutt39@gmail.com>
2026-04-28 18:37:18 +0500

    ruff check, fixed and formatted

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path("protected/", ProtectedView.as_view()),
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),
-    
-    # For (Getting, Updating and Deleting) the user by Id 
+    # For (Getting, Updating and Deleting) the user by Id
     path("users/<int:user_id>", UserDetailView.as_view()),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,7 @@
 # Create your views here.
-from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from users.serializers import UserSerializer, LoginSerializer
-from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
