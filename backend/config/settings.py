aa2bd465a11b9bb0994470f72e6db476374b5535
Abubakar Khawaja <abubakark1998@gmail.com>
2026-04-29 17:21:30 +0500

    feat: add chat app structure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,6 +59,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "auth.apps.AuthConfig",
     "users.apps.UsersConfig",
+    "chat.apps.ChatConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
