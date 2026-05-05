1edac5758514617727fd880d3ae4c71aa0a5a85e
Abubakar Khawaja <abubakark1998@gmail.com>
2026-05-05 05:43:27 +0500

    refactor: update authentication app file names

--- backend/apps/authentication/api/views/authentication.py
+++ backend/apps/authentication/api/views/authentication.py
@@ -2,7 +2,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.authentication.api.serializers.auth import LoginSerializer, RegisterSerializer
+from apps.authentication.api.serializers.authentication import (
+    LoginSerializer,
+    RegisterSerializer,
+)
 from apps.authentication.utils import get_tokens_for_user
 from apps.users.api.serializers.user import UserSerializer
 from utils.translations import t
